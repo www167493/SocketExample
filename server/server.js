c2dc6b9c6366847d2e5b24f11a39237e2c3d6605
www167493 <www167493@users.noreply.github.com>
2018-07-02 14:08:20 -0400

    Improved

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname, '../public');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const port= process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server); 
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+io.on('connection', (socket)=>{
+    console.log('Incoming User')
+
+    socket.on('disconnect',()=>{
+        console.log('User quit')
+    })
+});
+
+
+server.listen(port ,()=>{
     console.log(`Server is up on ${port}`)
 })
