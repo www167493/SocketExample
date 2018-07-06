053b6e2705b05f959beeeeca8278d1c16a93c6b1
Wesley Lau <wesleylau.wcl@gmail.com>
2018-07-06 15:00:18 -0400

    create generate utils

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const publicPath = path.join(__dirname, '../public');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message.js')
 const port= process.env.PORT || 3000;
 
 var app = express();
@@ -15,34 +16,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
     console.log('Incoming User');
 
-    socket.emit('newMessage',{
-        from: 'Admin',
-        text: 'Welcome noob'
-    });
+    socket.emit('newMessage',generateMessage('Admin', 'Welcome to the app'));
 
-    socket.broadcast.emit('newMessage',{
-        from: 'Admin',
-        text: 'Big Noob Joined ',
-        createdAt: new Date().getTime()
-    })
-
-    // socket.emit('newMessage', {
-    //     from:'mike@leesin.com',
-    //     created: 123123
-    // });
+    socket.broadcast.emit('newMessage',generateMessage('Admin','Big Noob Joined '))
 
     socket.on('createMessage', (message)=>{
         console.log('createMessage', message);
-        // io.emit('newMessage',{
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // })
-        socket.broadcast.emit('newMessage',{
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        })
+
+        io.emit('newMessage', generateMessage(message.from, messsage.text))
     })
 
     socket.on('disconnect',()=>{
