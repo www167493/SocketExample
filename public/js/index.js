cc22aa2365fb0087750884b3fd9a27f7a4271d70
Wesley Lau <wesleylau.wcl@gmail.com>
2018-07-07 15:05:45 -0400

    Add form

--- public/js/index.js
+++ public/js/index.js
@@ -15,11 +15,19 @@ socket.on('disconnect', function(){
 
 socket.on('newMessage',function(message){
     console.log('newMessage',message)
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
 })
 
-socket.emit('createMessage',{
-    from:'Lumpy',
-    text: 'High'
-},function(data){
-    console.log('A message is created',data)
+jQuery('#message-form').on('submit', function(e){
+    e.preventDefault();
+    
+    socket.emit('createMessage',{
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    },function(){
+
+    })
 })
