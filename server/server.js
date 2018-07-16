064f29e43aed9022516fdbf6ed16a4ad497c680e
Wesley Lau <wesleylau.wcl@gmail.com>
2018-07-16 15:27:12 -0400

    Styled the page

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ io.on('connection', (socket)=>{
 
         io.emit('newMessage', generateMessage(message.from, message.text));
 
-        callback('This is from the server');
+        callback();
     })
 
     socket.on('createLocationMessage',(coords)=>{
