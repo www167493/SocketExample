ea845541819a584b0f70d59166c7533c375b5997
Wesley Lau <wesleylau.wcl@gmail.com>
2018-07-16 17:40:56 -0400

    Implemented timestamp for chat

--- public/js/index.js
+++ public/js/index.js
@@ -14,20 +14,23 @@ socket.on('disconnect', function(){
 })
 
 socket.on('newMessage',function(message){
+    var formattedtime = moment(message.createdAt).format('h:mm a');
     console.log('newMessage',message)
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedtime}: ${message.text}`);
 
     jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message){
+    var formattedtime = moment(message.createdAt).format('h:mm a');
     var li=jQuery('<li></li>');
     var a =jQuery('<a target="_black">My Current location</a>');
 
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedtime}: `);
     a.attr('href', message.url);
 
+    
     li.append(a);
     jQuery('#messages').append(li);
 })
